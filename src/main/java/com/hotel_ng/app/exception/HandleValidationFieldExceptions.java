eff4bd7bdc9ffa0eb18113fa93b68a5db86500c9
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-19 21:17:46 +0100

    Agregar manejadores de excepciones para errores de validación y definir una excepción personalizada

---
+++ src/main/java/com/hotel_ng/app/exception/HandleValidationFieldExceptions.java
@@ -0,0 +1,32 @@
+package com.hotel_ng.app.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.hotel_ng.app.dto.ResponseDto;
+
+@RestControllerAdvice
+public class HandleValidationFieldExceptions {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        ResponseDto errorResponse = new ResponseDto();
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage("Errores de validación");
+        errorResponse.setErrors(errors);
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
---
+++ src/main/java/com/hotel_ng/app/exception/OurException.java
@@ -0,0 +1,9 @@
+package com.hotel_ng.app.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+
+}
