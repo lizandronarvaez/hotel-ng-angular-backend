419325733c1e839a82a8677bb8da0a896ee63305
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-19 21:21:08 +0100

    Agregar clase principal HotelNgApplication para iniciar la aplicación Spring Boot

---
+++ src/main/java/com/hotel_ng/HotelNgApplication.java
@@ -0,0 +1,12 @@
+package com.hotel_ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelNgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelNgApplication.class, args);
+    }
+}
