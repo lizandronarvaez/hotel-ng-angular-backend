4731833a60ab530748793f1e43efceaba2f5fab8
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-19 21:19:52 +0100

    Agregar implementación de AdminDetailsService para carga de detalles de usuario administrador

---
+++ src/main/java/com/hotel_ng/app/service/impl/AdminDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hotel_ng.app.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.hotel_ng.app.repository.AdminRepository;
+import com.hotel_ng.app.service.interfaces.AdminDetailsService;
+
+@RequiredArgsConstructor
+@Service
+public class AdminDetailsServiceImpl implements AdminDetailsService {
+
+    private final AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        String messageUserNotFound = String.format("El usuario %s no existe", username);
+        return adminRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(messageUserNotFound));
+    }
+    
+}
