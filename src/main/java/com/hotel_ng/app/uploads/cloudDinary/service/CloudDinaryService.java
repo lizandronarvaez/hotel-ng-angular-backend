d40c3c5edd1c4b14cc70a9f4f18d9e26ab72272d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-22 22:21:38 +0100

    refactor: Simplificar la interfaz CloudDinaryService y agregar manejo de excepciones

--- src/main/java/com/hotel_ng/app/uploads/cloudDinary/service/CloudDinaryService.java
+++ src/main/java/com/hotel_ng/app/uploads/cloudDinary/service/CloudDinaryService.java
@@ -1,16 +1,11 @@
 package com.hotel_ng.app.uploads.cloudDinary.service;
 
+import java.io.IOException;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public interface CloudDinaryService {
-    String uploadImage(MultipartFile file, String nameFolder);
-
-    String[] getFileNameParts(MultipartFile file);
-
-    void verifyFileSize(MultipartFile file);
-
+    String uploadImage(MultipartFile file, String nameFolder) throws IOException;
     void verifyFileFormat(String extFile);
-
-    String generateNewFileName(String nameFile, String extFile);
 
 }
