4d3c5f5148b4baa483b7e2751f50eae90daddf32
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-25 22:05:14 +0100

    refactor: Cambiar la anotación de CloudinaryProperties de @Configuration a @Component para mejorar la configuración de propiedades

--- src/main/java/com/hotel_ng/app/uploads/cloudDinary/configuration/CloudinaryProperties.java
+++ src/main/java/com/hotel_ng/app/uploads/cloudDinary/configuration/CloudinaryProperties.java
@@ -1,14 +1,14 @@
 package com.hotel_ng.app.uploads.cloudDinary.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "cloudinary")
 public class CloudinaryProperties {
     private String cloudName;
