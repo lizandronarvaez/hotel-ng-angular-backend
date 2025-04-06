304aca6d07af67b03a5080a7238958a5963ca4d8
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-06 22:00:52 +0200

    feat: Habilitar ejecución asíncrona en la aplicación principal

--- src/main/java/com/hotel_ng/HotelNgApplication.java
+++ src/main/java/com/hotel_ng/HotelNgApplication.java
@@ -2,7 +2,9 @@ package com.hotel_ng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class HotelNgApplication {
 
