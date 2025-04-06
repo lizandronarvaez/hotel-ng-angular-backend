312bf0d940dc07825440186f6294fa097fce3744
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-06 21:59:11 +0200

    feat: Añadir configuración CORS y seguridad para la aplicación

--- src/main/java/com/hotel_ng/app/config/MailConfig.java
+++ src/main/java/com/hotel_ng/app/config/MailConfig.java
@@ -1,4 +1,4 @@
-package com.hotel_ng.app.service.config;
+package com.hotel_ng.app.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
