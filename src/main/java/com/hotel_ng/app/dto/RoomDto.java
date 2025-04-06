bc2e9c15f1a3f30b58f4b4975d2b65ea69c5e217
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-06 22:03:49 +0200

    refactor: Eliminar importaciones innecesarias en RoomDto

--- src/main/java/com/hotel_ng/app/dto/RoomDto.java
+++ src/main/java/com/hotel_ng/app/dto/RoomDto.java
@@ -2,11 +2,8 @@ package com.hotel_ng.app.dto;
 
 import java.math.BigDecimal;
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hotel_ng.app.entity.ServiceRooms;
-
-import com.hotel_ng.app.enums.RoomType;
 import lombok.*;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
