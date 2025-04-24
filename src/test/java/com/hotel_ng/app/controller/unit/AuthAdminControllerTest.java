9bead1e15140aa0c880026a05ff1d29b444ffbea
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-24 20:20:24 +0200

    refactor: Agregar clases de prueba para AuthAdminController y UserController

---
+++ src/test/java/com/hotel_ng/app/controller/unit/AuthAdminControllerTest.java
@@ -0,0 +1,15 @@
+package com.hotel_ng.app.controller.unit;
+
+import org.junit.jupiter.api.Test;
+
+class AuthAdminControllerTest {
+
+    @Test
+    void registerAdmin() {
+
+    }
+
+    @Test
+    void loginAdmin() {
+    }
+}
---
+++ src/test/java/com/hotel_ng/app/controller/unit/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.hotel_ng.app.controller.unit;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class UserControllerTest {
+}
