ee98a108c054bff67d7feaec9444c25201272925
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-05-03 21:34:19 +0200

    feat: Agregar nuevos DTOs para administración y gestión de reservas, y eliminar DTOs obsoletos

--- src/main/java/com/hotel_ng/app/dto/ServiceRoomsDTO.java
+++ src/main/java/com/hotel_ng/app/dto/ServiceRoomsDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-public class ServiceRoomsDto {
+public class ServiceRoomsDTO {
     private long id;
     private String name;
 }
