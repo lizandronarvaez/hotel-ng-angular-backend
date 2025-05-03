6a938e70490ff459d39981eca9012cd961af136a
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-05-03 21:33:47 +0200

    refactor: Actualizar UserController para usar ResponseDTO en lugar de ResponseDto y RequestFormQuestionDTO en lugar de UserDto

--- src/main/java/com/hotel_ng/app/controller/UserController.java
+++ src/main/java/com/hotel_ng/app/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.hotel_ng.app.controller;
 
-import com.hotel_ng.app.dto.UserDto;
+import com.hotel_ng.app.dto.request.RequestFormQuestionDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import com.hotel_ng.app.dto.ResponseDto;
+import com.hotel_ng.app.dto.response.ResponseDTO;
 import com.hotel_ng.app.service.interfaces.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,46 +20,46 @@ public class UserController {
 
     @Tag(name = "Administradores")
     @GetMapping("/all-users")
-    public ResponseEntity<ResponseDto> getAllUsers() {
-        ResponseDto response = userService.getAllUsers();
+    public ResponseEntity<ResponseDTO> getAllUsers() {
+        ResponseDTO response = userService.getAllUsers();
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @Tag(name = "Administradores")
     @GetMapping("/get-user/{userId}")
-    public ResponseEntity<ResponseDto> getUserById(@PathVariable("userId") String userId) {
-        ResponseDto response = userService.getUserById(userId);
+    public ResponseEntity<ResponseDTO> getUserById(@PathVariable("userId") String userId) {
+        ResponseDTO response = userService.getUserById(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @Tag(name = "Administradores")
     @GetMapping("/get-profile-user-info")
-    public ResponseEntity<ResponseDto> getLoggedUserProfile() {
+    public ResponseEntity<ResponseDTO> getLoggedUserProfile() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
-        ResponseDto response = userService.getUserProfile(email);
+        ResponseDTO response = userService.getUserProfile(email);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @Tag(name = "Administradores")
     @GetMapping("/get-user-bookings/{userId}")
-    public ResponseEntity<ResponseDto> getHistoryBookingByUser(@PathVariable("userId") String userId) {
-        ResponseDto response = userService.getUserBookingHistory(userId);
+    public ResponseEntity<ResponseDTO> getHistoryBookingByUser(@PathVariable("userId") String userId) {
+        ResponseDTO response = userService.getUserBookingHistory(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @Tag(name = "Administradores")
     @DeleteMapping("/delete-user/{userId}")
-    public ResponseEntity<ResponseDto> deleteUser(@PathVariable("userId") String userId) {
-        ResponseDto response = userService.deleteUser(userId);
+    public ResponseEntity<ResponseDTO> deleteUser(@PathVariable("userId") String userId) {
+        ResponseDTO response = userService.deleteUser(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @Tag(name = "Público")
     @PostMapping("/form-contact")
-    public ResponseEntity<ResponseDto> receiveMessageUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<ResponseDTO> receiveMessageUser(@RequestBody RequestFormQuestionDTO formQuestionDTO) {
 
-        ResponseDto response = userService.formUserQuestion(userDto);
+        ResponseDTO response = userService.formUserQuestion(formQuestionDTO);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 }
