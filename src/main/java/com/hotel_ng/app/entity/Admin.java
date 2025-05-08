12690da8d5ca1b1e547aee265e4eb00ea3800495
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-05-08 20:12:29 +0200

    Implementar métodos de estado de cuenta en la clase Admin

--- src/main/java/com/hotel_ng/app/entity/Admin.java
+++ src/main/java/com/hotel_ng/app/entity/Admin.java
@@ -37,4 +37,24 @@ public class Admin implements UserDetails {
         return this.getEmail();
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
