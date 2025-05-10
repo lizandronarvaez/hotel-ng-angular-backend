73b3ffecb3f4c7d9ab58aa4d911c63a57670df2c
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-05-10 18:34:36 +0200

    Eliminando importación de la anotacion

--- src/test/java/com/hotel_ng/HotelNgApplicationTests.java
+++ src/test/java/com/hotel_ng/HotelNgApplicationTests.java
@@ -2,7 +2,6 @@ package com.hotel_ng;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class HotelNgApplicationTests {
